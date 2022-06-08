352d16b0c243d2a0268e76b6409900e6dd44152a
Ventustium <kevinlinuhung@gmail.com>
2022-06-08 14:23:17 +0700

    Added Login Using Google

--- app/src/main/java/com/ventustium/MyLnList/UI/Account.java
+++ app/src/main/java/com/ventustium/MyLnList/UI/Account.java
@@ -1,17 +1,42 @@
 package com.ventustium.MyLnList.UI;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.ventustium.MyLnList.R;
+
+import java.util.Objects;
 
 
 public class Account extends Fragment {
+
+    GoogleSignInClient gsc;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,7 +46,74 @@ public class Account extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == RESULT_OK){
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    handlerResult(task);
+                }
+            }
+        });
+
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestId().build();
+
+        gsc = GoogleSignIn.getClient(getActivity().getApplicationContext(), options);
+        SignInButton signInButton = v.findViewById(R.id.signInButton);
+        Button buttonLogout = v.findViewById(R.id.signOutButton);
+        signInButton.setOnClickListener(view1 -> {
+            Intent intent = gsc.getSignInIntent();
+            launcher.launch(intent);
+        });
+
+        buttonLogout.setOnClickListener(view1 -> {
+            gsc.signOut().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(getContext(), "Sign Out Success", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), "Sign Out Failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getContext());
+        if(account != null){
+            Toast.makeText(getContext(), account.getDisplayName(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void handlerResult(Task<GoogleSignInAccount> task) {
+        task.addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
+            @Override
+            public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+                Toast.makeText(getContext(), googleSignInAccount.getDisplayName(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "FAILED", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
